25c3e5997010adef57d09622dfeb8b6b9021c7ad
Bryan Matsuo <bryan.matsuo@gmail.com>
2010-04-29 23:34:13 -0700

    Initial commit.
    Doesn't DO anything; but, it parses the current AlsaState and will soon be
    able to alter it.
    
    After that it just needs to feed it back into alsactl.

---
+++ blib/lib/AlsaState.pm
@@ -0,0 +1,277 @@
+#!/usr/bin/env perl
+package AlsaState;
+
+use AlsaState::Parser;
+
+use Carp;
+
+my %control_names = (
+    'headphone' 
+        => {
+            'switch' => 'Headphone Playback Switch', 
+            'volume' => 'Headphone Playback Volume',
+        },
+    'speaker'
+        => {
+            'switch' => 'Speaker Playback Switch', 
+            'volume' => 'Speaker Playback Volume',
+        },
+    'master'
+        => {
+            'switch' => 'Master Playback Switch', 
+            'volume' => 'Master Playback Volume',
+        },
+    'PCM'
+        => {
+            'volume' => 'PCM Playback Volume',
+        },
+    'mic'
+        => {
+            'switch' => 'Capture Switch', 
+            'volume' => 'Capture Volume',
+            'boost' => 'Mic Boost',
+        },
+    'beep'
+        => {
+            'switch' => 'Beep Playback Switch', 
+            'volume' => 'Beep Playback Volume',
+        },
+);
+
+# Subroutine: control_name($connection, $type)
+# Type: INTERFACE SUB
+# Purpose: 
+#   Lookup specific connection controller names with simpler labels.
+# Returns: 
+#   Name of the $type (switch/volume/...) controller for $connection
+#   (speaker/headphone/...).
+sub control_name {
+    my ($connection, $type) = @_;
+
+    my $control_name_ref = $control_names{$connection};
+
+    return if !defined $control_name_ref;
+
+    my $control = $control_name_ref->{$type};
+
+    return if !defined $control;
+
+    return $control;
+}
+
+# Subroutine: alsa_state_string($state)
+# Type: INTERFACE SUB 
+# Purpose: 
+#   Format an object with structure as returned by parse_alsa_state as a
+#   valid alsa state configuration file that can be parsed by alsactl. 
+#   This is how modified settings will be loaded.
+# Returns: 
+#   The formatted alsa state string.
+sub state_string {
+    my ($class, $state) = @_;
+
+    my $state_str = "state.Intel {\n";
+
+    # Add the controls
+    for my $control (@{$state->{'controls'}}) {
+        $state_str .= "\tcontrol.$control->{'id'} {\n";
+
+        for my $comment (@{$control->{'comments'}}) {
+            $state_str 
+                .= "\t\tcomment.$comment->{'type'} $comment->{'value'}\n";
+        }
+
+        $state_str .= "\t\tiface $control->{'interface'}\n";
+
+        $state_str .= "\t\tname $control->{'name'}\n";
+
+        for my $value (@{$control->{'values'}}) {
+            $state_str .= "\t\tvalue";
+
+            $state_str .= ".$value->{'id'}" if defined $value->{'id'};
+
+            $state_str .= " $value->{'data'}\n";
+        }
+    }
+
+    $state_str .= "}\n";
+    return $state_str
+}
+
+
+# Subroutine: _alsactl_cmd($action, $card)
+# Type: INTERNAL UTILITY
+# Purpose: 
+#   Create a command to start alsactl
+# Returns: 
+#   Command as a list.
+sub _alsactl_cmd {
+    my ($action, $card) = @_;
+
+    my @cmd = ('alsactl', '-f', '-', $action);
+
+    push @cmd, $card if !defined $card && $card !~ m/\A\z/xms;
+
+    return @cmd;
+}
+
+# Subroutine: commit($state, $card)
+# Type: INTERFACE SUB
+# Purpose: 
+#   Commit an alsa state by giving to alsactl.
+# Returns: 
+#   undef
+sub commit {
+    my ($class, $state, $card) = @_;
+
+    my @alsactl_cmd = _alsactl_cmd('restore', $card);
+
+    my $state_str = state_string($state);
+
+    open my $alsactl, "|-", @alsactl_cmd
+        or croak("Couldn't open alsa to restore settings.\n");
+
+    print {$alsactl} $state_str;
+
+    close $alsactl;
+
+    return;
+}
+
+# Subroutine:   get(file => undef, card => undef)
+# Type: INTERFACE SUB
+# Purpose: 
+#   Read and parse the alsa state either from a file or by querying alsa.
+# Arguments:    file => Path to alsa state config 
+#                           (alsactl queried if undef)
+#               card => Card #, ID, or device path
+#                           (all returned if undef)
+# Returns: 
+#   The result of AlsaState::Parser->parse() on the raw string of the 
+#   requested alsa state.
+sub get { return AlsaState::Parser->parse(get_raw(@_)); }
+
+# Subroutine: get_raw(filename => undef, card => undef)
+# Type: INTERNAL SUB
+# Purpose: 
+#   Read in an alsa state file, or query alsactl for it's current state.
+# Arguments:    file => Path to alsa state config 
+#                           (alsactl queried if undef)
+#               card => Card #, ID, or device path
+#                           (all returned if undef)
+# Returns: 
+#   The configuration as a single string (with newlines '\n').
+sub get_raw {
+    my ($class, %option) = @_;
+
+    my $filename = $option{file};
+    my $card = $option{card};
+
+    my $state_fh;
+
+    # If not given a filename, then we need to get alsa's current info.
+    if (!defined $filename) {
+        # Create the alsactl query command.
+        my @alsactl_call = _alsactl_cmd("store", $card);
+
+        # Open a pipe from alsa.
+        open $state_fh, '-|', @alsactl_call
+            or croak("Couldn't run: ". join(q{ }, @alsactl_call)."\n");
+    } else {
+        # Open the alsa state file.
+        open $state_fh, "<", $filename 
+            or croak("Can't open alsa state file at '$filename'.\n");
+    }
+
+    # Slurp up the state contents.
+    my $state = do {local $/; <$state_fh>};
+
+    # Close the state file.
+    close $state_fh;
+
+    return $state;
+}
+
+# Subroutine: find_control_named($name, $alsa_state)
+# Type: INTERFACE SUB
+# Purpose: 
+#   Look through $alsa_state for the control with name equal to $name.
+# Returns: 
+#   Refenence to control named $name.
+sub find_control_named {
+    my ($class, $name, $alsa_state) = @_;
+
+    croak("find_contol_named(): name not given\n") if !defined $name;
+
+    croak("find_control_named(): alsa_state is not defined\n") if !defined $alsa_state;
+
+    my ($control) = grep {$_->{'name'} eq qq{'$name'}} @{$alsa_state->{'controls'}};
+
+    croak("Couldn't find control named $name in control list.\n") 
+        if !defined $control;
+
+    return $control;
+}
+
+# Subroutine: _is_valid_level($level)
+# Type: INTERNAL UTILITY
+# Purpose: 
+#   Check that a volume level is valid.
+# Returns: 
+#   True boolean value if $level is in interval [0,1];
+#   false otherwise.
+sub _is_valid_level {
+    my ($level) = @_;
+
+    return 0 <= $level && $level <= 1;
+}
+
+# Subroutine:   control_level($control, $level)
+#               control_level($control)
+# Type: INTERFACE SUB
+# Purpose: 
+#   Adjust the volume level of a control if given levels.
+#   Levels are percentages. The actual value assigned will 
+#   be the ceiling of the percentage value.
+#   TODO: Allow changing of left and right level separately.
+# Returns: 
+#   The level of the control.
+sub control_level {
+    my ($class, $control, $level) = @_;
+
+    my $name =  $control->{'name'};
+
+    if (defined $level && !_is_valid_level($level) ) {
+        croak("level $level of out bounds [0,1].\n");
+    }
+
+    if (! $control->{'type'} eq 'INTEGER' ) {
+        croak("control $name does not accept integer values.\n")
+    }
+
+    if (defined $level) {
+        my ($min,$max);
+
+        for my $comment (@{$control->{'comments'}}) {
+            if ($comment->{'ctype'} eq 'range') {
+                $comment->{'value'} =~ m/'(\d+) \s* - \s* (\d+)'/xms;
+                ($min, $max) = ($1, $2);
+            }
+        }
+
+        if (!defined $min || !defined $max) {
+            croak("can't find range of control $name.\n")
+        }
+
+        my $new_level = $min + $level * ($max - $min);
+
+        #print "setting values to $new_level\n";
+        for my $value (@{$control->{'values'}}) {
+            $value->{'data'} = $new_level;
+        }
+    }
+
+    return map {$_->{'data'}} @{$control->{'values'}};
+}
+
+return 1
---
+++ t/AlsaState-Parser.t
@@ -0,0 +1,152 @@
+# Before `make install' is performed this script should be runnable with
+# `make test'. After `make install' it should work as `perl pai.t'
+
+#########################
+
+# change 'tests => 1' to 'tests => last_test_to_print';
+
+use Test::More tests => 2;
+BEGIN { use_ok('AlsaState::Parser') };
+
+#########################
+
+# Create a typical alsa state
+my $state_1 =<<EOSTATE;
+state.Intel {
+	control.1 {
+		comment.access 'read write'
+		comment.type BOOLEAN
+		comment.count 2
+		iface MIXER
+		name 'Speaker Playback Switch'
+		value.0 true
+		value.1 true
+	}
+	control.2 {
+		comment.access 'read write'
+		comment.type INTEGER
+		comment.count 2
+		comment.range '0 - 64'
+		comment.dbmin -6300
+		comment.dbmax 100
+		iface MIXER
+		name 'Speaker Playback Volume'
+		value.0 63
+		value.1 63
+	}
+	control.3 {
+		comment.access 'read write'
+		comment.type BOOLEAN
+		comment.count 2
+		iface MIXER
+		name 'Headphone Playback Switch'
+		value.0 true
+		value.1 true
+	}
+	control.4 {
+		comment.access 'read write'
+		comment.type INTEGER
+		comment.count 2
+		comment.range '0 - 64'
+		comment.dbmin -6300
+		comment.dbmax 100
+		iface MIXER
+		name 'Headphone Playback Volume'
+		value.0 63
+		value.1 63
+	}
+	control.5 {
+		comment.access 'read write'
+		comment.type INTEGER
+		comment.count 2
+		comment.range '0 - 46'
+		comment.dbmin -1700
+		comment.dbmax 2900
+		iface MIXER
+		name 'Capture Volume'
+		value.0 46
+		value.1 46
+	}
+	control.6 {
+		comment.access 'read write'
+		comment.type BOOLEAN
+		comment.count 2
+		iface MIXER
+		name 'Capture Switch'
+		value.0 true
+		value.1 true
+	}
+	control.7 {
+		comment.access 'read write'
+		comment.type INTEGER
+		comment.count 2
+		comment.range '0 - 3'
+		comment.dbmin 0
+		comment.dbmax 3000
+		iface MIXER
+		name 'Mic Boost'
+		value.0 0
+		value.1 0
+	}
+	control.8 {
+		comment.access 'read write'
+		comment.type INTEGER
+		comment.count 2
+		comment.range '0 - 31'
+		comment.dbmin -3450
+		comment.dbmax 1200
+		iface MIXER
+		name 'Beep Playback Volume'
+		value.0 0
+		value.1 0
+	}
+	control.9 {
+		comment.access 'read write'
+		comment.type BOOLEAN
+		comment.count 2
+		iface MIXER
+		name 'Beep Playback Switch'
+		value.0 false
+		value.1 false
+	}
+	control.10 {
+		comment.access 'read write'
+		comment.type INTEGER
+		comment.count 1
+		comment.range '0 - 64'
+		comment.dbmin -6400
+		comment.dbmax 0
+		iface MIXER
+		name 'Master Playback Volume'
+		value 51
+	}
+	control.11 {
+		comment.access 'read write'
+		comment.type BOOLEAN
+		comment.count 1
+		iface MIXER
+		name 'Master Playback Switch'
+		value true
+	}
+	control.12 {
+		comment.access 'read write user'
+		comment.type INTEGER
+		comment.count 2
+		comment.range '0 - 255'
+		comment.tlv '0000000100000008ffffec1400000014'
+		comment.dbmin -5100
+		comment.dbmax 0
+		iface MIXER
+		name 'PCM Playback Volume'
+		value.0 255
+		value.1 255
+	}
+}
+EOSTATE
+# End of the 'typical' state output
+
+my $alsa_state_1 = eval {AlsaState::Parser->parse($state_1)};
+
+ok(!$@, "parse a 'typical' state");
+
+1;
